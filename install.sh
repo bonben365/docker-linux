347372034f4d7014ca9e2dcfebb863bcb8732de4
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-03-27 21:57:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ then
     sudo yum-config-manager \
         --add-repo \
         https://download.docker.com/linux/centos/docker-ce.repo
-    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    sudo systemctl start docker
     sudo systemctl start docker
 fi
