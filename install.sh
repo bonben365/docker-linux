ce9ab8c8378533c8751acb0701b62f640981a782
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-03-28 00:49:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,3 +29,56 @@ then
     sudo systemctl start docker
     sudo systemctl status docker
 fi
+
+if [ $version == "Debian" ]
+then
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl gnupg
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl start docker
+    sudo systemctl status docker
+fi
+
+if [ $version == "Fedora" ]
+then
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl start docker
+    sudo systemctl status docker
+fi
+
+if [ $version == "Red" ]
+then
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl start docker
+    sudo systemctl status docker
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
